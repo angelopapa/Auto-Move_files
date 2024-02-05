9de58ef22237b3da664fc13ff9bd97de1e16b767
angelopapa <angelo.papa50@gmail.com>
2024-02-05 18:21:46 +0100

    first commit

---
+++ autodesfiles.py
@@ -0,0 +1,109 @@
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
+
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# ! compila la destinazione dei file
+# ? es. in souce_dir -> "C:/Utenti/NomeUtente/Download"
+source_dir = ""
+dest_dir_sound = ""
+dest_dir_music = ""
+dest_dir_video = "" 
+dest_dir_image = "" 
+dest_dir_documents = ""
+
+# ? FILE IMMAGINI
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+# ? FILE VIDEO
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+# ? FILE AUDIO
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+# ? FILE DOCUMENTI
+document_extensions = [".doc", ".docx", ".odt",
+                       ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+
+def make_unique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    # * SE IL FILE ESISTE, AGGIUNGE UN NUMERO ALLA FINE DEL NOME FILE
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+
+    return name
+
+
+def move_file(dest, entry, name):
+    if exists(f"{dest}/{name}"):
+        unique_name = make_unique(dest, name)
+        oldName = join(dest, name)
+        newName = join(dest, unique_name)
+        rename(oldName, newName)
+    move(entry, dest)
+
+
+class MoverHandler(FileSystemEventHandler):
+    # ? QUESTA FUNZIONE VERRÀ ESEGUITA OGNI VOLTA C'È UN CAMBIAMENTO IN "source_dir"
+    # ? .upper serve per non perdere file con estensioni maiuscole
+    def on_modified(self, event):
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                self.check_audio_files(entry, name)
+                self.check_video_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_document_files(entry, name)
+
+    def check_audio_files(self, entry, name):  # * Controllo file audio
+        for audio_extension in audio_extensions:
+            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                if entry.stat().st_size < 10_000_000 or "SFX" in name:  # ? 10Megabytes
+                    dest = dest_dir_sound
+                else:
+                    dest = dest_dir_music
+                move_file(dest, entry, name)
+                logging.info(f"Moved audio file: {name}")
+
+    def check_video_files(self, entry, name):  # * Controllo file video
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                move_file(dest_dir_video, entry, name)
+                logging.info(f"Moved video file: {name}")
+
+    def check_image_files(self, entry, name):  # * Controllo file immagini
+        for image_extension in image_extensions:
+            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                move_file(dest_dir_image, entry, name)
+                logging.info(f"Moved image file: {name}")
+
+    def check_document_files(self, entry, name):  # * Controllo documenti
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                move_file(dest_dir_documents, entry, name)
+                logging.info(f"Moved document file: {name}")
+
+
+# ! NON CAMBIARE IL CODICE 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
